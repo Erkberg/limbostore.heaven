9695907e91098b7706d74349d1f03662e2159df2
Erkberg <erisch01@web.de>
2019-04-27 14:02:21 +0200

    player rotation

--- limbostore.heaven/Assets/Scripts/Player/PlayerMovement.cs
+++ limbostore.heaven/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Transform sprite;
 
     public float moveSpeed = 1f;
     public bool movementEnabled = true;    
@@ -14,6 +15,11 @@ public class PlayerMovement : MonoBehaviour
         if(movementEnabled)
         {
             Move();
+
+            if(rb.velocity != Vector2.zero)
+            {
+                Rotate(rb.velocity);
+            }
         }
     }
 
@@ -23,5 +29,11 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxis(InputStrings.VerticalAxis);
         Vector2 movementAmount = new Vector2(horizontal, vertical);
         rb.velocity = movementAmount * moveSpeed;
+    }
+
+    private void Rotate(Vector3 movement)
+    {
+        float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+        sprite.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
