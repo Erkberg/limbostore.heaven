e1097c94a42ee79835afb4abefb27bb475eb6871
Kasimir Blust <blust@b-rex.de>
2019-04-27 16:10:10 +0200

    add death types and death screen

--- limbostore.heaven/Assets/Scripts/Game/DeathType.cs
+++ limbostore.heaven/Assets/Scripts/Game/DeathType.cs
@@ -6,6 +6,9 @@ namespace Game
     public class DeathType : ScriptableObject
     {
         public string title;
+        [TextArea(2, 16)]
+        public string description;
+        public AudioClip deathClip;
         
         public int rewardFirstDeath = 10;
         public int rewardDefault = 1;
---
+++ limbostore.heaven/Assets/Scripts/Game/UI/DeathScreen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game;
+using TMPro;
+using UnityEngine;
+
+public class DeathScreen : MonoBehaviour
+{
+    public Canvas canvas;
+
+    public CanvasGroup selfGroup;
+
+    public TMP_Text titleTextElement, descriptionTextElement;
+    public AudioSource audioSource;
+
+    public Animator animator;
+    
+    void Awake()
+    {
+    }
+    
+    void DisplayDeathType(DeathType type, bool firstTimeDeath)
+    {
+        titleTextElement.SetText(type.title);
+        descriptionTextElement.SetText(type.description + "<br><br> You were rewarded " +
+           (firstTimeDeath ? +type.rewardFirstDeath : type.rewardDefault).ToString("N0") +
+           " lives.");
+        
+        if(type.deathClip != null)
+            audioSource.PlayOneShot(type.deathClip);
+        
+        animator.SetBool("Active", true);
+    }
+
+    void Close()
+    {
+        animator.SetBool("Active", false);
+    }
+}
