7fd94cbc19d2baed21fdcc9bcc533014cd1f4fd4
Erkberg <erisch01@web.de>
2019-04-27 17:07:53 +0200

    interaction basics, prefabs for sudden death, collectables and shop interactions

---
+++ limbostore.heaven/Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    private InteractionArea currentInteractionArea;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown(InputStrings.InteractButton))
+        {
+            if(currentInteractionArea)
+            {
+                currentInteractionArea.TriggerInteraction();
+            }
+        }
+    }
+
+    public void OnEnterInteractionArea(InteractionArea area)
+    {
+        currentInteractionArea = area;
+        Highlight();
+    }
+
+    public void OnExitInteractionArea(InteractionArea area)
+    {
+        if(currentInteractionArea == area)
+        {
+            currentInteractionArea = null;
+            Dehighlight();
+        }
+    }
+
+    private void Highlight()
+    {
+
+    }
+
+    private void Dehighlight()
+    {
+
+    }
+}
---
+++ limbostore.heaven/Assets/Scripts/World/Collectable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public string id;
+
+    private void Start()
+    {
+        if(GameManager.Current.collectables.HasCollectable(id))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Collect()
+    {
+        GameManager.Current.collectables.AddCollectable(id);
+        Destroy(gameObject);
+    }
+}
