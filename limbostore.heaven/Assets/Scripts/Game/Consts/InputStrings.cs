df40e2562856bf55922a9312c4a29ce6e7b612eb
Erkberg <erisch01@web.de>
2019-04-27 15:11:44 +0200

    player running and sneaking, prepare player animator

--- limbostore.heaven/Assets/Scripts/Game/Consts/InputStrings.cs
+++ limbostore.heaven/Assets/Scripts/Game/Consts/InputStrings.cs
@@ -6,4 +6,8 @@ public static class InputStrings
 {
     public const string HorizontalAxis = "Horizontal";
     public const string VerticalAxis = "Vertical";
+
+    public const string RunButton = "Run";
+    public const string SneakButton = "Sneak";
+    public const string InteractButton = "Interact";
 }
---
+++ limbostore.heaven/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator animator;
+
+    private const string movingBool = "moving";
+    private const string runningBool = "running";
+    private const string sneakingBool = "sneaking";
+
+    public void SetMoving(bool moving)
+    {
+        animator.SetBool(movingBool, moving);
+    }
+
+    public void SetRunning(bool running)
+    {
+        animator.SetBool(runningBool, running);
+    }
+
+    public void SetSneaking(bool sneaking)
+    {
+        animator.SetBool(sneakingBool, sneaking);
+    }
+}
