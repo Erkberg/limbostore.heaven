6303ba8239401b41d247aa550105c5ed34c97f64
Erik Schulze <erik@studio-fizbin.de>
2019-04-27 12:55:52 +0200

    Created a currency manager.

---
+++ limbostore.heaven/Assets/CurrencyManager.cs
@@ -0,0 +1,45 @@
+﻿public class CurrencyManager
+{
+    private int availableCoins = 0;
+    
+    public CurrencyManager()
+    {
+        availableCoins = 0;
+    }
+
+    public int Amount => availableCoins;
+    
+    /// <summary>
+    /// checks if you can afford the item that costs a amount of money
+    /// </summary>
+    /// <param name="amount"></param>
+    /// <returns></returns>
+    public bool CanAfford(int amount)
+    {
+        return availableCoins >= amount;
+    }
+
+    /// <summary>
+    /// Purchase something. Return false if you can't afford it.
+    /// </summary>
+    /// <param name="amount"></param>
+    /// <returns></returns>
+    public bool Purchase(int amount)
+    {
+        if (!CanAfford(amount))
+            return false;
+        availableCoins -= amount;
+        return true;
+    }
+
+    /// <summary>
+    /// Store money into the availableCoins. Returns the total amount stored.
+    /// </summary>
+    /// <param name="amount"></param>
+    /// <returns></returns>
+    public int Receive(int amount)
+    {
+        availableCoins += amount;
+        return availableCoins;
+    }
+}
