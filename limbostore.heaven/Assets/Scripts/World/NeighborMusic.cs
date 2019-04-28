318352063418b8b6da3231b25ea4163725765e6c
Kasimir Blust <blust@b-rex.de>
2019-04-28 18:51:50 +0200

    add neighbor sound

---
+++ limbostore.heaven/Assets/Scripts/World/NeighborMusic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeighborMusic : MonoBehaviour
+{
+    private AudioSource loudSource, quietSource;
+
+    void Start()
+    {
+        if (GameManager.Current.skillz.CanDo(SkillType.Ohrstoepsel))
+        {
+            quietSource.Play();
+        }
+        else
+        {
+            loudSource.Play();
+        }
+    }
+
+    private void Update()
+    {
+        if (GameManager.Current.PlayerLocked)
+        {
+            this.enabled = false;
+            quietSource.Stop();
+            loudSource.Stop();
+        }
+    }
+}
